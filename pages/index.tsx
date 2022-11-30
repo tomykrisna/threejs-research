e13f53ca6958d27bf2d13ea85f030e27b67d7708
tomykrisna <tommykrisna7@gmail.com>
2022-11-30 17:11:11 +0700

    update 3d

--- pages/index.tsx
+++ pages/index.tsx
@@ -37,14 +37,13 @@ function Box(props: any) {
 export default function Home() {
     return (
         <Canvas>
-            <ambientLight />
             <Scene/>
-            {/*<ambientLight intensity={0.5} />*/}
-            {/*<spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />*/}
-            {/*<pointLight position={[-10, -10, -10]} />*/}
-            {/*<Box position={[-1.2, 0, 0]} />*/}
+            <ambientLight intensity={0.5}/>
+            <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+            <pointLight position={[-10, -10, -10]} />
             {/*<Box position={[1.3, 0, 2]} />*/}
-            {/*<OrbitControls />*/}
+            <Box position={[1.3, 0, 2]} />
+            <OrbitControls />
         </Canvas>
     )
 }
